cbad49da53fd41ee70bb61eaa32c91ddda68c797
root <kualangm@gmail.com>
2023-06-22 05:10:18 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers since 0 up to 0
+ * Return: The numbers since 0 up to 0
+ */
+
+void print_numbers(void)
+
+{
+	int n;
+
+	for (n = 0; n <= 0; n++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+}
+
