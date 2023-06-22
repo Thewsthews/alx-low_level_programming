0e7d2e0ce04292ca754bbf9f2a9bfa23b9476dc1
root <kualangm@gmail.com>
2023-06-22 03:08:30 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if longer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int k;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
